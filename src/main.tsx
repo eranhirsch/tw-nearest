40aa5b48d0a5a8c017a5171bcd3684c4b5aa9f54
Eran Hirsch <eranhirsch@gmail.com>
2023-06-22 21:52:54 +0000

    fix some lints

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App.tsx";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.querySelector("#root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
