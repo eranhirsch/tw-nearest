fbfe1f1882ba2b09369d0f74d6b4a6831413af13
Eran Hirsch <eranhirsch@gmail.com>
2023-06-23 10:51:04 +0000

    color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 export function App() {
-  return null;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center overflow-hidden">
+      <main>
+        <form>
+          <label className="flex items-center gap-2">
+            Color: <input type="color" />
+          </label>
+        </form>
+      </main>
+    </div>
+  );
 }
