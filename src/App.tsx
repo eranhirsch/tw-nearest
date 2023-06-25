02a8d410e8c712f7358e3a8a53a416d92b7291ff
Eran Hirsch <eranhirsch@gmail.com>
2023-06-25 20:05:18 +0000

    prepare for better output

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,66 @@
-import { PropsWithChildren, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { twNearest } from "./twNearest";
 
 export function App() {
-  const [color, setColor] = useState("#000000");
-
-  const closest = useMemo(() => twNearest(color), [color]);
+  const [color, setColor] = useState<string>();
 
   return (
     <div className="flex h-screen w-screen items-center justify-center overflow-hidden">
-      <main className="flex h-full w-full items-center justify-evenly gap-8 overflow-hidden">
-        <form>
-          <label className="flex items-center gap-2">
-            Color:{" "}
-            <input
-              type="color"
-              onChange={({ currentTarget: { value } }) => {
-                setColor(value);
-              }}
-            />
-          </label>
-        </form>
-        <section className="flex h-1/2 w-96">
-          <div className="h-full flex-1" style={{ backgroundColor: color }} />
-          <ol className="h-full flex-[3_3] overflow-hidden overflow-y-auto">
-            {closest.map(({ color, names, distance }) => (
-              <li key={color}>
-                {names.length > 1 ? (
-                  <ul>
-                    {names.map(([colorName, shade]) => (
-                      <li key={colorName}>
-                        <Color
-                          colorName={colorName}
-                          shade={shade}
-                          color={color}
-                        >
-                          {distance}
-                        </Color>
-                      </li>
-                    ))}
-                  </ul>
-                ) : (
-                  <Color
-                    colorName={names[0][0]}
-                    shade={names[0][1]}
-                    color={color}
-                  >
-                    {distance.toFixed(2)}
-                  </Color>
-                )}
-              </li>
-            ))}
-          </ol>
-        </section>
+      <main className="flex h-full w-full items-center justify-evenly gap-12 overflow-hidden p-12">
+        <ColorSelector onChange={setColor} />
+        {color !== undefined && <Results color={color} />}
       </main>
     </div>
+  );
+}
+
+function ColorSelector({
+  onChange,
+}: {
+  readonly onChange: (value: string) => void;
+}): JSX.Element {
+  return (
+    <form>
+      <label className="flex items-center gap-2">
+        Color:{" "}
+        <input
+          type="color"
+          onChange={({ currentTarget: { value } }) => {
+            onChange(value);
+          }}
+        />
+      </label>
+    </form>
+  );
+}
+
+function Results({ color }: { readonly color: string }): JSX.Element {
+  const closest = useMemo(() => twNearest(color), [color]);
+
+  return (
+    <ol className="flex h-full w-1/2 flex-wrap items-center justify-center gap-1 overflow-hidden overflow-y-auto">
+      {closest.map(({ color, names }) => (
+        <>
+          {names.length > 1 ? (
+            <>
+              {names.map(([colorName, shade]) => (
+                <li key={colorName}>
+                  <Color colorName={colorName} shade={shade} color={color} />
+                </li>
+              ))}
+            </>
+          ) : (
+            <li key={color}>
+              <Color
+                colorName={names[0][0]}
+                shade={names[0][1]}
+                color={color}
+              />
+            </li>
+          )}
+        </>
+      ))}
+    </ol>
   );
 }
 
@@ -61,22 +68,18 @@ function Color({
   colorName,
   shade,
   color,
-  children,
-}: PropsWithChildren<{
+}: {
   readonly colorName: string;
   readonly shade: number | undefined;
   readonly color: string;
-}>): JSX.Element {
+}): JSX.Element {
   return (
-    <article className="flex w-full gap-2">
-      <div className="w-1/2" style={{ backgroundColor: color }} />
-      <div className="flex items-center justify-center p-2">
-        <pre className="flex-none rounded bg-gray-200 p-1 font-mono text-xs font-semibold text-gray-800">
-          {colorName}
-          {shade !== undefined && <>-{shade}</>}
-        </pre>
-      </div>
-      {children}
+    <article
+      className="flex items-center justify-center rounded p-1 font-mono text-xs font-semibold"
+      style={{ backgroundColor: color }}
+    >
+      {colorName}
+      {shade !== undefined && <>-{shade}</>}
     </article>
   );
 }
