4abd9d3d9b73e93f36c825d19e59a3270e171e01
Eran Hirsch <eranhirsch@gmail.com>
2023-06-25 20:15:37 +0000

    bigger and better

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useMemo, useState } from "react";
 import { twNearest } from "./twNearest";
+import { asLAB } from "./color_spaces/cielab";
+import { asCIEXYZ } from "./color_spaces/ciexyz";
+import { asSRGB } from "./color_spaces/srgb";
 
 export function App() {
   const [color, setColor] = useState<string>();
 
   return (
     <div className="flex h-screen w-screen items-center justify-center overflow-hidden">
-      <main className="flex h-full w-full items-center justify-evenly gap-12 overflow-hidden p-12">
+      <main className="flex h-full w-full items-center justify-evenly gap-20 overflow-hidden p-20">
         <ColorSelector onChange={setColor} />
         {color !== undefined && <Results color={color} />}
       </main>
@@ -38,7 +41,7 @@ function Results({ color }: { readonly color: string }): JSX.Element {
   const closest = useMemo(() => twNearest(color), [color]);
 
   return (
-    <ol className="flex h-full w-1/2 flex-wrap items-center justify-center gap-1 overflow-hidden overflow-y-auto">
+    <ol className="flex h-full flex-wrap items-center justify-center gap-3 overflow-hidden overflow-y-auto">
       {closest.map(({ color, names }) => (
         <>
           {names.length > 1 ? (
@@ -73,9 +76,12 @@ function Color({
   readonly shade: number | undefined;
   readonly color: string;
 }): JSX.Element {
+  const { l } = asLAB(asCIEXYZ(asSRGB(color)));
   return (
     <article
-      className="flex items-center justify-center rounded p-1 font-mono text-xs font-semibold"
+      className={`flex items-center justify-center rounded p-2 font-mono text-sm font-semibold ${
+        l > 50 ? "text-neutral-950" : "text-neutral-50"
+      }`}
       style={{ backgroundColor: color }}
     >
       {colorName}
