8910ffbbb6f821710bcf5a375e36c1a84625560a
Eran Hirsch <eranhirsch@gmail.com>
2023-06-27 10:16:43 +0000

    rgb

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,7 +1,14 @@
 import { ChangeEvent, useEffect, useRef, useState } from "react";
+import invariant from "tiny-invariant";
 import { CSS_COLORS_RAW } from "../pallettes/csscolors";
 
 const REGEX_RGB_HEXADECIMAL = /^#[\da-f]{3,6}$/;
+
+// Accept percentage or numbers, and accept any number of spaces, legacy commas
+// (with any number of spaces after them), or tailwind specific underscores
+// (which replace spaces).
+const REGEX_RGB =
+  /^rgb\( *(\d+%?)(?:(?: *[ ,] *)|_)(\d+%?)(?:(?: *[ ,] *)|_)(\d+%?) *\)$/;
 
 export function Selector({
   onChange,
@@ -22,7 +29,8 @@ export function Selector({
     currentTarget: { value },
   }: ChangeEvent<HTMLInputElement>) => {
     const normalized = value.trim().toLowerCase();
-    const color = fromNamedColor(normalized) ?? fromHex(normalized);
+    const color =
+      fromNamedColor(normalized) ?? fromHex(normalized) ?? fromRGB(normalized);
     if (color === undefined) {
       return;
     }
@@ -76,3 +84,29 @@ const fromHex = (possibleHex: string): string | undefined =>
         `#${possibleHex[1]!}${possibleHex[1]!}${possibleHex[2]!}${possibleHex[2]!}${possibleHex[3]!}${possibleHex[3]!}`
       : undefined
     : undefined;
+
+const fromRGB = (possibleRGB: string): string | undefined => {
+  const match = possibleRGB.match(REGEX_RGB);
+  if (match === null || match.length !== 4) {
+    return undefined;
+  }
+
+  try {
+    return `#${match
+      .slice(1)
+      .map((raw) => toHexPart(raw))
+      .join("")}`;
+  } catch {
+    return;
+  }
+};
+
+function toHexPart(raw: string | undefined): string {
+  invariant(raw !== undefined, "Missing section");
+  const rawNumber = Number.parseInt(raw);
+  const asNumber = raw.endsWith("%")
+    ? Math.floor((rawNumber / 100) * 0xff)
+    : rawNumber;
+  invariant(asNumber >= 0 && asNumber <= 0xff, `Out of range ${asNumber}`);
+  return asNumber.toString(16).padStart(2, "0");
+}
