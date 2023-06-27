3f967f00d539b832185f6b7957c176f74fba40b3
Eran Hirsch <eranhirsch@gmail.com>
2023-06-27 08:57:11 +0000

    sync input and picker

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { CSS_COLORS_RAW } from "../pallettes/csscolors";
 
 export function Selector({
@@ -6,6 +6,16 @@ export function Selector({
 }: {
   readonly onChange: (value: string) => void;
 }): JSX.Element {
+  const textInputReference = useRef<HTMLInputElement>(null);
+  const [color, setColor] = useState<string>();
+
+  useEffect(() => {
+    if (color === undefined) {
+      return;
+    }
+    onChange(color);
+  }, [color, onChange]);
+
   const handleTextChange = ({
     currentTarget: { value },
   }: ChangeEvent<HTMLInputElement>) => {
@@ -14,23 +24,36 @@ export function Selector({
     if (color === undefined) {
       return;
     }
-    onChange(color);
+    setColor(color);
+  };
+
+  const handlePickerChange = ({
+    currentTarget: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setColor(value);
+
+    const { current } = textInputReference;
+    if (current === null) {
+      return;
+    }
+
+    current.value = value;
   };
 
   return (
-    <form className="flex h-10 items-center rounded p-1 ring ring-neutral-100">
+    <form className="flex h-10 items-center rounded p-2 ring ring-neutral-100">
       <input
-        className="text-md font-mono outline-none"
-        placeholder="Enter a color..."
+        ref={textInputReference}
+        className="text-md font-mono text-neutral-500 outline-none"
+        placeholder="CSS Color"
         type="text"
         onChange={handleTextChange}
       />
       <input
+        value={color}
         className="h-10 w-10 rounded-md bg-transparent p-2"
         type="color"
-        onChange={({ currentTarget: { value } }) => {
-          onChange(value);
-        }}
+        onChange={handlePickerChange}
       />
     </form>
   );
