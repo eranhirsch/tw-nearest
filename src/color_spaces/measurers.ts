3ec6d90656422b660a1119474b68243d48d443c2
Eran Hirsch <eranhirsch@gmail.com>
2023-06-28 11:53:23 +0000

    cyclic euclide

--- src/color_spaces/measurers.ts
+++ src/color_spaces/measurers.ts
@@ -1,7 +1,7 @@
 import {
   lab as d3Lab,
   rgb as d3Rgb,
-  hcl as d3Hcl,
+  lch as d3Lch,
   hsl as d3Hsl,
   cubehelix as d3Cubehelix,
 } from "d3-color";
@@ -12,9 +12,9 @@ type MEASURER = (pivot: string) => (target: string) => number;
 
 export const MEASURERS = {
   lab: euclideanDistance(d3Lab, ["l", "a", "b"]),
-  hcl: euclideanDistance(d3Hcl, ["h", "c", "l"]),
+  lch: euclideanDistance(d3Lch, ["l", "c", "h"], { part: "h", max: 360 }),
   redmean: (pivot) => (target) => redmean(pivot, target),
-  hsl: euclideanDistance(d3Hsl, ["h", "s", "l"]),
+  hsl: euclideanDistance(d3Hsl, ["h", "s", "l"], { part: "h", max: 360 }),
   rgb: euclideanDistance(d3Rgb, ["r", "g", "b"]),
   cubehelix: euclideanDistance(d3Cubehelix, ["h", "s", "l"]),
 } as const satisfies Readonly<Record<string, MEASURER>>;
--- src/utils/euclideanDistance.ts
+++ src/utils/euclideanDistance.ts
@@ -2,6 +2,7 @@ export const euclideanDistance =
   <K extends string>(
     parser: (color: string) => Readonly<Record<K, number>>,
     parts: readonly K[],
+    cyclic?: { part: K; max: number },
   ) =>
   (pivot: string) => {
     const parsedPivot = parser(pivot);
@@ -11,12 +12,15 @@ export const euclideanDistance =
       for (const part of parts) {
         const pivotPart = parsedPivot[part];
         const targetPart = parsedTarget[part];
-        distance += Math.pow(
+        distance +=
           Number.isNaN(pivotPart) || Number.isNaN(targetPart)
             ? 0
-            : pivotPart - targetPart,
-          2,
-        );
+            : Math.min(
+                Math.pow(pivotPart - targetPart, 2),
+                cyclic !== undefined && part === cyclic.part
+                  ? Math.pow(cyclic.max - pivotPart - targetPart, 2)
+                  : Number.POSITIVE_INFINITY,
+              );
       }
       return Math.pow(distance, 0.5);
     };
