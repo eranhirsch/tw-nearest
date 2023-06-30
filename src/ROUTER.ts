3d8283f91f2bcdfeb95f599f028f8bf80eeb1d69
Eran Hirsch <eranhirsch@gmail.com>
2023-06-30 08:18:37 +0000

    rename all

--- src/ROUTER.ts
+++ src/ROUTER.ts
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { App } from "./components/App";
+import { App } from "./routes/App";
 
 export const ROUTER = createBrowserRouter(
   [
